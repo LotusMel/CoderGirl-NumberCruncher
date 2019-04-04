b24787edf675d1fa55fd80023ec5f50e51fbc9b8
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-04-03 22:33:37 -0500

    1st push

--- NumberCruncher/Program.cs
+++ NumberCruncher/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace NumberCruncher
 {
     public static class Program
     {
+        List<int> myNumbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
         public static void Main()
         {
-            //Create a list of ints 
-            //Call SumOfEvenNumbers in NumberUtility and pass this list in
-            //Pring out the result
+            NumberUtility numberUtility = new NumberUtility();
+
+            
+            Console.WriteLine(numberUtility.SumOfEvenNumbers(sum, myNumbers));
             Console.ReadLine();
         }
+
+       
     }
 }
--- NumberUtility/NumberUtility.cs
+++ NumberUtility/NumberUtility.cs
@@ -2,11 +2,41 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace NumberUtility
+namespace NumberCruncher
 {
-    public static class NumberUtility
+    public class NumberUtility
     {
-        //create a static method SumOfEvenNumbers 
+        List<int> myNumbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        public int SumOfEvenNumbers(int sum, List<int> myNumbers)
+        {
+
+
+            foreach (int number in myNumbers)
+                
+            if (number % 2 == 0)
+            
+                    sum += number;
+                    sum++;
+                return sum;
+                     
+                
+            
+        }
+
+        public void SumOfEvenNumbers(object sum, object myNumbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+        
+
+
+
+
+
+        //create a 
         //it should take a list of ints and returns the sum of all even number in that list
     }
 }
