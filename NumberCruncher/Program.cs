58ecc9602b06c42ca73e58bdf9680a5786b4935d
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-04-06 02:00:42 -0500

    Issue with test

--- NumberCruncher/Program.cs
+++ NumberCruncher/Program.cs
@@ -6,14 +6,14 @@ namespace NumberCruncher
 {
     public static class Program
     {
-        List<int> myNumbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
         public static void Main()
         {
+            int sum = 0;
             NumberUtility numberUtility = new NumberUtility();
 
             
-            Console.WriteLine(numberUtility.SumOfEvenNumbers(sum, myNumbers));
+            Console.WriteLine(numberUtility.SumOfEvenNumbers(sum));
             Console.ReadLine();
         }
 
--- NumberUtility/NumberUtility.cs
+++ NumberUtility/NumberUtility.cs
@@ -8,34 +8,21 @@ namespace NumberCruncher
     {
         List<int> myNumbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-        public int SumOfEvenNumbers(int sum, List<int> myNumbers)
+        public int SumOfEvenNumbers(int sum)
         {
 
 
             foreach (int number in myNumbers)
-                
-            if (number % 2 == 0)
-            
+
+                if (number % 2 == 0)
+
                     sum += number;
-                    sum++;
-                return sum;
-                     
-                
             
+            return sum;
+
+
+
         }
-
-        public void SumOfEvenNumbers(object sum, object myNumbers)
-        {
-            throw new NotImplementedException();
-        }
-
-        
-        
-
-
-
-
-
         //create a 
         //it should take a list of ints and returns the sum of all even number in that list
     }
